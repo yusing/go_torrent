4d13e68711df0db9e7045c3d4660e1d5c602abcc
yusing <yusing.wys@gmail.com>
2023-08-10 14:20:27 +0800

    v0.0.2 alpha added mobile view and working on android

--- client.go
+++ client.go
@@ -14,6 +14,7 @@ var savePath string
 var dataPath string
 
 func loadLastSession() {
+	log.Println("[Torrent-Go] Loading session...")
 	// torrent list is data/*.json
 	if files, err := os.ReadDir(dataPath); err != nil {
 		log.Printf("[Torrent-Go] Error reading data dir: %s", err)
@@ -27,23 +28,26 @@ func loadLastSession() {
 				ReadMetadataAndAdd(file.Name()[:len(file.Name())-5])
 			}
 		}
-		log.Println("Session loaded")
+		log.Println("[Torrent-Go] Session loaded")
 	}
 }
 
 //export InitTorrentClient
 func InitTorrentClient(savePathCStr *C.char) {
+	log.Println("[Torrent-Go] Initializing...")
 	if torrentClient != nil {
 		return // Already initialized, maybe flutter hot reload?
 	}
 	savePath = C.GoString(savePathCStr)
 	dataPath = path.Join(savePath, "data")
-	os.MkdirAll(dataPath, 0755)
+	if err := os.MkdirAll(dataPath, 0644); err != nil {
+		log.Printf("[Torrent-Go] Error creating data dir: %s", err)
+		os.Exit(1)
+	}
 	config := torrent.NewDefaultClientConfig()
 	config.NoDHT = false
 	config.NoUpload = false
 	config.DataDir = savePath
-	config.Seed = true
 	config.DisableIPv6 = true
 	torrentClient, _ = torrent.NewClient(config)
 	loadLastSession()
