32e785cc29a85a8f72da79ae2c5082da368af93c
yusing <yusing.wys@gmail.com>
2023-08-27 01:01:09 +0800

    improvement and initial support for youtube

--- build_windows.bat
+++ build_windows.bat
@@ -2,7 +2,7 @@
 set CGO_ENABLED=1
 set GOARCH=amd64
 
-go build -o build/windows/x64/libtorrent_go.dll -buildmode=c-shared -ldflags="-s -w"
+go build -o build\windows\torrent_go.dll -buildmode=c-shared -ldflags="-s -w"
 
 REM set GOARCH=386
 REM go build -o build/windows/x86/libtorrent_go.dll -buildmode=c-shared
